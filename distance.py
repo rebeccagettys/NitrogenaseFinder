b73737adec4aeedcbed5d24b7fd3a18be560a34c
rebeccagettys <rebecca.gettys@students.olin.edu>
2016-05-04 22:49:58 -0400

    dictionary change-over :)

--- distance.py
+++ distance.py
@@ -9,20 +9,17 @@ def levenshtein (a_full,b_full, a_start=0, b_start= 0, known_memo=None):
     b_start: the index in b to start from
     returns: minimum levenshtein distance between the two strings starting from
              the specified positions a_start and b_start as an integer
+    Credit for understanding/example code:  https://programmingpraxis.com/2014/09/12/levenshtein-distance/
     >>> levenshtein ("alphabet", "alphabetsoup")
     4
     """
-    # a = a_full[a_start:]
-    # b = b_full[b_start:]
     if known_memo == None:
         known_memo = {}
-    # https://programmingpraxis.com/2014/09/12/levenshtein-distance/
 
     if a_start >= len(a_full):
         return len(b_full)-b_start+1
     if b_start >= len(b_full):
         return len(a_full)- a_start+1 # so far, list compatible
-    #known_memo = {a,b:0, b,a:0, a:b, b:a} # do I need b:a
     if (a_start,b_start) in known_memo:
         return known_memo[(a_start,b_start)]
     else:
@@ -31,5 +28,4 @@ def levenshtein (a_full,b_full, a_start=0, b_start= 0, known_memo=None):
         length3 = levenshtein(a_full, b_full, a_start+1, b_start+1, known_memo) + (a_full[a_start] != b_full[b_start])  #if a 0 and b 0 are not equal, that is = 1, otherwise 0 if false
 
         known_memo [(a_start,b_start)] = min (length1, length2, length3)
-    #print known_memo
     return known_memo[(a_start,b_start)]
